c78a6b0346571ffa6e88f6b7dee1f886d0a3ba14
Lucas Lima <48462708+limalucas99@users.noreply.github.com>
2022-03-13 19:04:03 -0300

    useEffect - Tutorial

--- src/App.js
+++ src/App.js
@@ -1,37 +1,55 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react'; // useEffect faz o trabalho de componentDidMount, componentDidUpdate e componentDidWillUnmount
 import './App.css';
-import { useState } from 'react';
 
 function App() {
-  const [reverse, setReverse] = useState(false);
   const [counter, setCounter] = useState(0);
-  const reverseClass = reverse ? 'reverse' : '';
+  const [counter2, setCounter2] = useState(0);
 
-  const handleClick = () => {
-    setReverse(!reverse);
+  // // componentDidUpdate - executa toda vez que o componente atualiza
+  // useEffect(() => {
+  //   console.log('componentDidUpdate');
+  // });
+
+  // // componentDidMount - executa 1x
+  // useEffect(() => {
+  //   console.log('componentDidMount');
+  // }, []);
+
+  // // Com dependência - executa toda vez que a dependência mudar
+  // useEffect(() => {
+  //   console.log('Contador mudou para', counter); // Se for utilizado qualquer elemento do estado, é uma dependência, então deve ser passada para o Array de dependências
+  // }, [counter]);
+
+  // // Com dependência - executa toda vez que a dependência mudar
+  // useEffect(() => {
+  //   console.log('Contador2 mudou para', counter2); // Se for utilizado qualquer elemento do estado, é uma dependência, então deve ser passada para o Array de dependências
+  // }, [counter2]);
+
+  // // Com dependência - executa toda vez que a dependência mudar
+  // useEffect(() => {
+  //   console.log(`Counter 1 ${counter} - Counter 2 ${counter2}`); // Se for utilizado qualquer elemento do estado, é uma dependência, então deve ser passada para o Array de dependências
+  // }, [counter, counter2]); // toda a variável utilizada dentro do useEffect deve ser passado como uma dependência
+
+  const eventFn = () => {
+    console.log('oi');
   };
 
-  const handleIncrement = () => {
-    // setCounter(counter + 1);
-    setCounter((prevCounter) => prevCounter + 1); // recomendado utilizar essa sintaxe de callback
-  };
+  useEffect(() => {
+    document.querySelector('h1')?.addEventListener('click', eventFn);
+
+    // componentWillMount - Limpeza
+    return () => {
+      document.querySelector('h1').removeEventListener('click', eventFn);
+    };
+  });
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
-        <h1>Contar: {counter}</h1>
-        <p>
-          <button type="button" onClick={handleIncrement}>
-            Increment
-          </button>
-        </p>
-        <p>
-          <button type="button" onClick={handleClick}>
-            Reverse
-          </button>
-        </p>
-      </header>
+      <h1>C1: {counter}</h1>
+      <h1>C2: {counter2}</h1>
+      <button onClick={() => setCounter(counter + 1)}>+</button>
+      <br />
+      <button onClick={() => setCounter2(counter2 + 1)}>+</button>
     </div>
   );
 }

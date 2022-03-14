5752cac222802a519e298a3d8a819a4802cfb8a7
Lucas Lima <48462708+limalucas99@users.noreply.github.com>
2022-03-14 19:33:54 -0300

    useMemo - tutorial

--- src/App.js
+++ src/App.js
@@ -1,36 +1,52 @@
 import P from 'prop-types';
-import React, { useCallback, useMemo, useState } from 'react'; // useEffect faz o trabalho de componentDidMount, componentDidUpdate e componentDidWillUnmount
+import { useEffect, useMemo, useState } from 'react';
 import './App.css';
 
-const Button = ({ incrementButton }) => {
-  // React.memo - salva o componente na memória
-  console.log('Filho Renderizou!');
-  return <button onClick={() => incrementButton(10)}>+</button>;
+const Post = ({ post }) => {
+  console.log('filho renderizou');
+  return (
+    <div key={post.id} className="post">
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </div>
+  );
 };
 
-Button.propTypes = {
-  incrementButton: P.func,
+Post.propTypes = {
+  post: P.shape({
+    id: P.number,
+    title: P.string,
+    body: P.string,
+  }),
 };
 
 function App() {
-  const [counter, setCounter] = useState(0);
+  const [posts, setPosts] = useState([]);
+  const [value, setValue] = useState('');
 
-  const incrementCounter = useCallback((num) => {
-    // A função fica memoizada em cache
-    setCounter((c) => c + num);
-  }, []); // Faz com que a função fique salva na memória e não seja recriada toda vez que o componente pai (App) é renderizado
+  console.log('Pai renderizou!');
 
-  console.log('Pai Renderizou!');
-
-  const btn = useMemo(() => <Button incrementButton={incrementCounter} />, [incrementCounter]);
-  // Tudo que for usado e for externo deve estar no array de dependências
+  // componentDidMount
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((r) => r.json())
+      .then((r) => setPosts(r));
+  }, []);
 
   return (
     <div className="App">
-      <p>Teste 3</p>
-      <h1>C1: {counter}</h1>
-      {btn}
-      {/* O componente não é re-renderizado desnecessariamente */}
+      <p>
+        <input type="search" value={value} onChange={(e) => setValue(e.target.value)} />
+      </p>
+      {useMemo(() => {
+        return (
+          posts.length > 0 &&
+          posts.map((post) => {
+            return <Post key={post.id} post={post} />;
+          })
+        );
+      }, [posts])}
+      {posts.length <= 0 && <p>Ainda não existem posts. </p>}
     </div>
   );
 }

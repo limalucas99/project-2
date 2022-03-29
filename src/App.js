bca4fc9cd5ede664f594eee1b32e6a5beb2691de
Lucas Lima <48462708+limalucas99@users.noreply.github.com>
2022-03-28 23:03:29 -0300

    UseReducer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
+import { useReducer } from 'react';
 import './App.css';
-import { AppContext } from './contexts/AppContext';
-import { Div } from './components/Div';
+
+const globalState = {
+  title: 'O título que contexto',
+  body: 'O body do contexto',
+  counter: 0,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'muda': {
+      console.log('Chamou muda com', action.payload);
+      return { ...state, title: action.payload };
+    }
+    case 'inverter': {
+      console.log('Chamou inverter');
+      const { title } = state;
+      return { ...state, title: title.split('').reverse().join('') };
+    }
+  }
+  console.log('NENHUMA ACTION ENCONTRADA');
+  return { ...state };
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, globalState);
+  const { counter, title } = state;
   return (
-    <AppContext>
-      <Div />
-    </AppContext>
+    <div>
+      <h1>
+        {title} {counter}
+      </h1>
+      <button onClick={() => dispatch({ type: 'muda', payload: new Date().toLocaleString('pt-BR') })}>Click</button>
+      <button onClick={() => dispatch({ type: 'inverter' })}>Invert</button>
+    </div>
   );
 }
 
